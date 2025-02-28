25cc230b96f41ccfb1fbd938aebf575926fe7a2b
ThanhTrangTran <103062661+ThanhTrangTran@users.noreply.github.com>
2025-02-28 16:07:59 -0600

    Add files via upload

--- Completed Queries.sql
+++ Completed Queries.sql
@@ -1,62 +1,45 @@
+-- Select database
+SET search_path TO rxnorm;
+
 -- Renal Disease:
 -- Q1. List the drug names of the class of drugs for first-line treatment of CKD, diabetes, or HTN if albuminuria is present.
-
-WITH drug AS (
-SELECT rc.str AS drug_name, rc.code AS atc_code
-FROM rxnconso rc
-JOIN 
-rxnsat rs
-ON rc.code = rs.code
-WHERE rc.code LIKE ANY(ARRAY['C09A%','C09C%'])   --ATC codes for ACEi and ARB respectively
-AND rs.atn = 'ATC_LEVEL'
-AND rs.atv = '5'
-),
-atc AS (
-SELECT rc.code AS atc_code, rc.str AS atc_class
-FROM rxnconso rc
-JOIN
-rxnsat rs
-ON rs.code = rc.code
-WHERE rc.code LIKE ANY(ARRAY['C09A%','C09C%'])   --ATC codes for ACEi and ARB respectively
-AND rs.atn = 'ATC_LEVEL'
-AND rs.atv = '4'
-)
-SELECT drug.*, atc.atc_class
-FROM drug
-LEFT JOIN atc
-ON LEFT(drug.atc_code, 5) = atc.atc_code
-ORDER BY drug.atc_code
+SELECT rc1.str AS drug_name
+	, rc1.code AS atc_code
+	, rc2.str AS atc_class
+FROM rxnconso rc1
+LEFT JOIN rxnconso rc2
+ON LEFT(rc1.code, 5) = rc2.code
+JOIN rxnsat rs1
+ON rc1.code = rs1.code
+WHERE rc1.code LIKE ANY(ARRAY['C09A%','C09C%'])   --ATC codes for ACEi and ARB respectively
+	AND rs1.atn = 'ATC_LEVEL'
+	AND rs1.atv = '5'
+ORDER BY atc_code
 ;
 
 
 -- Blood:
 -- Q1. Which drug classes are blood thinners?
--- 1st Option:
-SELECT code as drug_class_code, str as drug_class_name
-FROM rxnconso
-WHERE code LIKE 'B01A_'  -- ATC code for antithrombotic agents
-ORDER BY drug_class_code;
--- 2nd Option:
-SELECT rc.code as drug_class_code, rc.str as drug_class_name
+SELECT rc.code as drug_class_code, MIN(rc.str) as drug_class_name
 FROM rxnconso rc
-JOIN rxnsat rs
+INNER JOIN rxnsat rs
 ON rc.rxcui = rs.rxcui
-WHERE rc.code LIKE 'B01%'  -- ATC code for antithrombotic agents
-AND rs.atn = 'ATC_LEVEL'
-AND rs.atv = '4'
-ORDER BY drug_class_code;
+WHERE rc.code LIKE 'B01%'  -- ATC group code for antithrombotic agents
+	AND rs.atn = 'ATC_LEVEL'
+	AND rs.atv = '4'	    -- Select for pharmacological or therapeutic subgroups
+GROUP BY 1
+ORDER BY 1;
 
 --Q2. List NDCs of the drug that requires consistent dietary vitamin K?
-SELECT rc.str as drug_name
-	, rs.atv as ndc
+SELECT rc.str as drug_name, rs.atv as ndc
 FROM rxnconso rc
 JOIN
 rxnsat rs
 ON rc.rxcui = rs.rxcui
 WHERE rc.str ILIKE 'warfarin%'
-AND rs.atn = 'NDC'
-AND rs.sab = 'RXNORM'
-AND rc.tty = 'SCD'
+	AND rs.atn = 'NDC'
+	AND rs.sab = 'RXNORM' 	--Normalized NDCs
+	AND rc.tty = 'SCD'    	--Name, strength, and form
 ORDER BY drug_name;
 
 --Q3. List RXCUI and NDC for one drug that should be administered if a patient develops heparin-induced thrombocytopenia.
@@ -91,6 +74,23 @@ FROM rxnconso
 WHERE str LIKE '%statin'
 AND code LIKE 'C10AA__';
 
+--Q3. What are the different brand names for the generic medication clopidogrel?
+WITH rxcuis AS (
+	SELECT DISTINCT rc.rxcui AS generic_rxcui
+		, rc.str AS generic_name
+		, rr.rxcui2 AS brand_rxcui
+	FROM rxnconso rc
+	INNER JOIN rxnrel rr
+	ON rc.rxcui = rr.rxcui1
+	WHERE rc.str LIKE 'clopidogrel'
+	AND rr.rela = 'tradename_of'
+	)
+SELECT DISTINCT rc.str AS brand_name
+FROM rxcuis
+INNER JOIN rxnconso rc
+ON rxcuis.brand_rxcui = rc.rxcui
+;
+
 
 -- Neuro & Psych:
 --Q1. Select antianxiolytics.
@@ -104,4 +104,13 @@ WHERE atn = 'ATC_LEVEL'
 AND atv = '5'
 AND rs.code LIKE 'N05BA%'  -- Code for antianxiolytics
 AND rc.tty = 'IN'
+;
+
+
+-- General questions:
+-- Q1. Find the salt forms of ephedrine.
+SELECT rxcui, str AS drug_name
+FROM rxnconso
+WHERE str ILIKE 'ephedrine%'
+AND tty = 'PIN'   -- Specify for salt or isomer forms
 ;
